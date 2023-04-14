e940ca3e04682a664eb6104154bef8fe85144e7a
steeper <portland.steeper@gmail.com>
2023-04-14 00:12:48 -0400

    Revert "but at 1:13:00, refused to load the stylesheet, need to debug"
    This reverts commit 4da80995e0233e32896f29bb8e149d9277505469.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
 import { register, reset } from '../features/auth/authSlice'
-import Spinner from '../components/Spinner'
+// import Spinner from '../components/Spinner'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -59,9 +59,9 @@ function Register() {
   }
 
 
-  if (isLoading) {
-    return <Spinner />
-  }
+  // if (isLoading) {
+  //   return <Spinner />
+  // }
 
 
 
