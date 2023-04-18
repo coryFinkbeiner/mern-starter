5cd66bd6ab2a1decc3f7752c43b9069b6be40c3f
steeper <portland.steeper@gmail.com>
2023-04-18 01:21:29 -0400

    kasj

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -6,7 +6,7 @@ function GoalItem({ goal }) {
 
   return (
     <div className='goal'>
-      <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+      {/* <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div> */}
       <h2>{goal.text}</h2>
       <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
         X
